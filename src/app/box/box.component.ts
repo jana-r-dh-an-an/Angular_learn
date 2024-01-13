cc0f15613eacbcd89ff8bd52ea301b92ce68f485
janardhanan <rsjana305@gmail.com>
2024-01-13 09:59:31 +0530

    angular learn

---
+++ src/app/box/box.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-box',
+  standalone: true,
+  imports: [],
+  templateUrl: './box.component.html',
+  styleUrl: './box.component.css'
+})
+export class BoxComponent {
+
+  showAlert(){
+     
+  }
+
+}
